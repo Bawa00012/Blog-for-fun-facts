6830a6334f88e0945f7dec8bf87407c1f64761fa
Daniel Bawa <danielbawa000@gmail.com>
2025-09-08 15:24:38 +0100

    first commit

---
+++ page.js
@@ -0,0 +1,23 @@
+let shine =' Space'
+
+document.getElementById('silence').innerHTML = shine; 
+
+let darkness = 'Astronauts’ footprints on the Moon will likely last millions of years because the Moon has no atmosphere, wind, or water to erode them away! The tracks left during the Apollo missions in the late 1960s and early 1970s are still there today—and will probably outlast the pyramids on Earth.';
+
+document.getElementById('silencer').innerHTML = darkness;
+
+let light ='Animals'
+
+document.getElementById('county').innerHTML = light;
+
+let heavy ='Octopuses are some of the most incredible creatures in the ocean. They have three hearts—two pump blood to the gills, while the third pumps it to the rest of the body. What’s even cooler is that their blood is blue, not red, because it uses copper instead of iron to carry oxygen—this helps them survive in the cold, deep sea.';
+
+document.getElementById('country').innerHTML = heavy;
+
+let might ='Weather'
+
+document.getElementById('egg').innerHTML = might;
+
+let sight ='Tornadoes are nature’s most powerful spinning storms, often called “twisters.” They form when warm, moist air from the ground meets cold, dry air from above, creating unstable conditions. If the winds start rotating strongly enough inside a thunderstorm, a funnel cloud can form and touch down on the ground—that’s when it becomes a tornado.'
+
+document.getElementById('indomie').innerHTML = sight;
